08c561f1a6e15a3462af614b05fdfb1b96cc28cf
Shiven Juneja <keymouseit@bitbucket.org>
2018-10-01 12:01:54 +0530

    error remove

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Red from './containers/red'
 import App from './App';
 
 
